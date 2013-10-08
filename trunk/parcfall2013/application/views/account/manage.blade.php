f77d1f88b7a3034936f800a5215c14a9109bf5a2
mrlunchbox777@gmail.com <mrlunchbox777@gmail.com@fde8f477-8489-ecd5-8870-be133e43484a>
2013-10-08 13:44:00 +0000

    Updated the code to be congruent with other pages.

--- trunk/parcfall2013/application/views/account/manage.blade.php
+++ trunk/parcfall2013/application/views/account/manage.blade.php
@@ -1,6 +1,6 @@
 <!-- <div id="manageTabs"> how it used to be, remove for conformity -->
 	<ul class="breadcrumb">
-		<li><a href="#employees">Main</a></li>
+		<li class="active">Main</li>
 		<?php //if(Auth::user()->can('reportGeneration') || Auth::user()->can('reportTemplate')){ ?>
 		<!-- <li><a href="#reports">Reports</a></li> -->
 		<?php //} ?>
@@ -17,7 +17,7 @@
 <div id="employees">
 	{{$employeeContent}}
 </div>
-
+<!-- Old Styles & Scripts
 @section('styles')
 <style type="text/css">
 </style>
@@ -26,4 +26,52 @@
 <script type="text/javascript">
 	$("#manageTabs").tabs();
 </script>
+@endsection
+-->
+@section('styles')
+	<link rel="stylesheet" href="/styles/css/selectList.css">
+	<link rel="stylesheet" href="/styles/css/ui.css">
+<style>
+.column1{
+	width: 48%;
+    float:left;
+    border-right:solid 1px;
+    padding: 0 4px;
+    white-space: nowrap;
+}
+.column2{
+    float:left;
+    padding: 0 4px;
+    white-space: nowrap;
+}
+
+.modalHeader{
+	background-color:#00A305;
+	font-size:medium;
+	color:white;
+}
+.ui-datepicker-trigger{
+	margin-top:-10px;
+}
+.control-label{
+	font-size:medium;
+}
+.controls{
+	margin-left:2%;
+}
+.listColumn{
+	background-color:#00A305;
+	color:white;
+	font-size:medium;
+}
+.searchButton{
+	margin-top:-8px;
+}
+#buttons button{
+	margin-left:0;
+}
+input[type='radio']{
+		margin-top:-2px;
+	}
+</style>
 @endsection
