b6c6e0cf310c498a1870193c4cb7fee6dbad10a1
rybor16@gmail.com <rybor16@gmail.com@fde8f477-8489-ecd5-8870-be133e43484a>
2013-10-17 13:46:57 +0000

    camel case the table name in the migration add_alt_id_formquestion

--- trunk/parcfall2013/application/migrations/2013_10_09_041324_add_alt_id_formquestion.php
+++ trunk/parcfall2013/application/migrations/2013_10_09_041324_add_alt_id_formquestion.php
@@ -1,17 +1,17 @@
 <?php
 
-class Add_Alt_Id_Formquestion {    
+class Add_Alt_Id_Formquestion {
 
 	public function up()
     {
-		Schema::table('formquestion', function($table) {
+		Schema::table('FormQuestion', function($table) {
 			$table->integer('alt_id')->unsigned()->default(NULL);
 		});
-    }    
+    }
 
 	public function down()
     {
-		Schema::table('formquestion', function($table) {
+		Schema::table('FormQuestion', function($table) {
 			$table->drop_column('alt_id');
 		});
     }
