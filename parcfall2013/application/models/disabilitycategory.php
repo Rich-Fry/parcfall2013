aab6fd71915c3a5be2f4b6159f7728da2c92f915
adamlemmon101812 <adamlemmon101812@gmail.com>
2013-11-05 15:09:10 +0000

    Finished ERS Report. It saves out as SpeedERS_YYYY_MM_DD.xlsx

--- parcfall2013/application/models/disabilitycategory.php
+++ parcfall2013/application/models/disabilitycategory.php
@@ -6,7 +6,8 @@ class DisabilityCategory extends Eloquent
 	
 	public static function getDisabilityCategories()
 	{
-		return DisabilityCategory::select('disabilitycategory.npa_disability_category as npa_disability_category,
-		 disabilitycategory.ers_disability_category as ers_disability_category')->get();
+		return DB::query('select * from disabilitycategory');
+		//return DisabilityCategory::select('disabilitycategory.npa_disability_category as npa_disability_category,
+		// disabilitycategory.ers_disability_category as ers_disability_category')->get();
 	}
 }
--- parcfall2013/application/models/worklocation.php
+++ parcfall2013/application/models/worklocation.php
@@ -6,10 +6,6 @@ class WorkLocation extends Eloquent
 	
 	public static function getWorkLocations()
 	{
-		return WorkLocation::select('worklocation.work_location_code as work_location_code,
-		 worklocation.primary_work_location_name as primary_work_location_name,
-		 worklocation.primary_work_location_zip as primary_work_location_zip,
-		 worklocation.primary_work_location_type as primary_work_location_type,
-		 worklocation.primary_industry as primary_industry')->get();
+		return DB::query('select * from worklocation');
 	}
 }
