ccbafed5170c65c62e3d35aff0997cc0ad92386c
mrlunchbox777@gmail.com <mrlunchbox777@gmail.com@fde8f477-8489-ecd5-8870-be133e43484a>
2013-11-07 15:17:00 +0000

    Took out the unneeded download function.

--- trunk/parcfall2013/application/models/ersreport.php
+++ trunk/parcfall2013/application/models/ersreport.php
@@ -19,24 +19,6 @@ class ErsReport extends Eloquent
 		//$objWriter->save('SpeedERS.xlsx');
 	}
 	
-	public static function download()
-	{
-		header ('Content-Type: application/vnd.ms-excel; charset=utf-8');
-		header ('Content-Disposition: attachment; filename="ERS Reports\SpeedERS_'.date("Y_m_d").'.xlsx"'); 
-		header("Expires: 0");
-		header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
-		header("Cache-Control: private",false);
-		
-		$objReader = PHPExcel_IOFactory::createReader('Excel2007');
-		$objPHPExcel = new PHPExcel();
-		$objPHPExcel = $objReader->load('ERS Reports\SpeedERS_'.date("Y_m_d").'.xlsx');
-		
-		//$objPHPExcel->load('ERS Reports\SpeedERS_'.date("Y_m_d").'.xlsx');
-		$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
-		$objWriter->save('php://output');
-		//readfile('ERS Reports\SpeedERS_'.date("Y_m_d").'.xlsx');
-	}
-	
 	private static function generateDemographics($objPHPExcel)
 	{
 		// Create a new worksheet
