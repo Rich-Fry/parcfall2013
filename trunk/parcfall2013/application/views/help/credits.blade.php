1ae12f7c32befa40e8b0d8bd63ee355bc77a1aa5
rybor16@gmail.com <rybor16@gmail.com@fde8f477-8489-ecd5-8870-be133e43484a>
2013-11-07 15:55:08 +0000

    Added credits pages

---
+++ trunk/parcfall2013/application/views/help/credits.blade.php
@@ -0,0 +1,10 @@
+<h2>Proudly created by these students from Weber State University</h2>
+<br><br>
+<h4>Fall Semester 2013</h4>
+<ul>
+	<li>Ryan Born</li>
+	<li>Andrew Gurney</li>
+	<li>Adam Lemmon</li>
+	<li>Andrew Shoell</li>
+	<li>Emily Smith</li>
+</ul>
