85df0c002b5902ab0a4dfa82ae9765f91321d455
gurneyad@gmail.com <gurneyad@gmail.com@fde8f477-8489-ecd5-8870-be133e43484a>
2014-01-25 04:54:51 +0000

    Added credits for Spring Semester

--- trunk/parcfall2013/application/views/help/credits.blade.php
+++ trunk/parcfall2013/application/views/help/credits.blade.php
@@ -1,6 +1,14 @@
 <h2>Proudly created by these students from Weber State University</h2>
 <br><br>
 <h3>Version 3.1</h3>
+
+<h4>Spring Semester 2014</h4>
+<ul>
+	<li>Andrew Gurney</li>
+	<li>Andrew Shoell</li>
+	<li>Emily Smith</li>
+</ul>
+<br><br>
 <h4>Fall Semester 2013</h4>
 <ul>
 	<li>Ryan Born</li>
